b5374e8bb30617076376945620293daffc8e7013
Tyler McAnally <tylerdmcanally@gmail.com>
2026-08-27 13:06:55 -0500

    Fix Day 3 save review on mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "aft-workout-tracker-v40";
-const ASSETS=['./','./index.html','./styles.css?v=40','./program-config.js?v=40','./cloud-config.js?v=40','./cloud-sync.js?v=40','./app.js?v=40','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png','./icons/apple-touch-icon.png'];
+const CACHE = "aft-workout-tracker-v41";
+const ASSETS=['./','./index.html','./styles.css?v=41','./program-config.js?v=41','./cloud-config.js?v=41','./cloud-sync.js?v=41','./app.js?v=41','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
